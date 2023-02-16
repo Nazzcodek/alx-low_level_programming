c0f89a588d5c9ab1c5ffbba497ac83c24e0d7651
Nazzcodek <bellnas09@gmail.com>
2023-02-16 06:45:18 -0800

    initial commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main( void )
+{
+  /* print */
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  /*return 0 means success*/
+  return(0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* main to return 0*/
+
+int main()
+         {
+           printf("Size of char: %2d bytes \n", sizeof(char));
+             printf("Size of int: %2d bytes \n", sizeof(int));
+             printf("Size of long int: %2d bytes \n", sizeof(long int));
+             printf("Size of long long int: %2d bytes \n", sizeof(long long int));
+             printf("Size of float: %2d bytes \n", sizeof(float));
+             return(0)
+	  }

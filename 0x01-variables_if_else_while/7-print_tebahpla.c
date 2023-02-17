9fe2efaa7994e0fd6fe2b4307ffcef1f2adec27e
Nazzcodek <bellnas09@gmail.com>
2023-02-17 14:35:31 -0800

    initial commit

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,13 +8,14 @@
 
 int main(void)
 {
-  char alp[26] = "abcdefghijklmnopqrstuvwxwz";
-  int i;
+	char alp[26] = "abcdefghijklmnopqrstuvwxwz";
 
-  for (i = 0; i > 26; --i)
-    {
-      putchar(alp[i]);
-    }
-    putchar('\n');
-    return (0);
+	int i;
+
+	for (i = 0; i > 26; i--)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
 }

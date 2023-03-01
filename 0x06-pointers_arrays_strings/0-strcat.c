e8750d7ee6dcb4078a3a146286d52fc3dbbd76aa
Nazzcodek <bellnas09@gmail.com>
2023-03-01 06:27:56 -0800

    initial commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - concantenate two strings
+ *
+ * @dest: destination string
+ * @src: source string
+ * Return: return the pointer to string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int count = 0, count2 = 0;
+
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+
+	while (count2 >= 0)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
+	return (dest);
+}

f90785cc766a0a5d1bc40710193979d85930c2d9
Nazzcodek <bellnas09@gmail.com>
2023-03-09 07:26:38 -0800

    initial commit

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * wildcmp - Entry Point
+ *
+ * @s1: input one
+ * @s2: input two
+ *
+ * Return: 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	return (0);
+}

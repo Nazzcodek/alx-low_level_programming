3a347ef0c13f8c48e0b0f46484f184c7b87c5288
Nazzcodek <bellnas09@gmail.com>
2023-03-13 08:20:24 -0700

    task 3 and 4 initial commit

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid - A function that return a pointer
+ * to a 2 dimmessional array
+ *
+ * @width: width of the array
+ * @height: height of the array
+ *
+ * Return: 0 alaways success
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **iarray;
+	int n, i;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	iarray = molloc(sizeof(char) * height);
+
+	if (iarray == NULL)
+	{
+		free(iarray);
+		return (NULL);
+	}
+
+	for (i < 0; i < height; i++)
+	{
+		iarray[i] = malloc(sizeof(char) * width);
+		if (iarray == NULL)
+		{
+			free(iarray);
+			return (NULL);
+		}
+
+		for (n = 0; n < width; n++)
+		{
+			iarray[i][n] = 0;
+		}
+	}
+	return (iarray);
+}
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - a function that free 2D grid
+ *
+ * @grid: grud to be free
+ * @height: height of the grid
+ *
+ * Return: 0 always sucess
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid != NULL || height != 0)
+	{
+		for (i = 0; i < height; i++)
+		{
+			free(grid[i]);
+		}
+		free(grid)
+	}
+}

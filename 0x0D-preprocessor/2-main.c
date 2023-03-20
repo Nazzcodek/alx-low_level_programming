10cd7c6d72f7018b7c58697bd5b6ef0c3146974c
Nazzcodek <bellnas09@gmail.com>
2023-03-20 04:57:04 -0700

    initial commit of all task

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Alawys (0) success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef _FUNCTION_LIKE_MACRO_H_
+#define _FUNCTION_LIKE_MACRO_H_
+
+#define SUM(a, b) ((a) + (b))
+
+#endif /* _FUNCTION_LIKE_MACRO_H_ */

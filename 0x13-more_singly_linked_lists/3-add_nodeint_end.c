1546b4d4fe5e1ac0cca28c03ab48c6083457c63f
Nazzcodek <bellnass09@gmail.com>
2023-04-04 04:09:01 -0700

    task 3 done

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -33,7 +33,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		while (last_n->next != NULL)
 			last_n = last_n->next;
 
-		last_n->next = new
+		last_n->next = new_n;
 	}
 	return (new_n);
 }

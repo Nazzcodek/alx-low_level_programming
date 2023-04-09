2b984b02b7bb9491a9e8927fdb2f2e62b276ad94
Nazzcodek <bellnass09@gmail.com>
2023-04-09 14:54:47 -0700

    task 2 corrected

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	op = open(filename, O_WRONLY | O_APPEND);
-	write = write(o, text_content, len);
+	wri = write(op, text_content, len);
 
 	if (op == -1 || wri == -1)
 		return (-1);

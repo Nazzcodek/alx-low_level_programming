6a9ec014961d55503d5d0fd245059cadf3666b4b
Nazzcodek <bellnass09@gmail.com>
2023-04-09 08:14:20 -0700

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * create_file - A function that create file
+ *
+ * @filename: name of the file
+ * @text_content: content of the file
+ *
+ * Return: (-1) if failed, filename
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int op, wri, len = 0;
+
+	if (!filename)
+		return (-1);
+
+	op = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (op == -1)
+		return (-1);
+
+	if (!text_content)
+	{
+		for (; text_content[len];)
+			len++;
+	}
+	wri = write(op, text_content, len);
+
+	if (op == -1 || wri  == -1)
+		return (-1);
+
+	close(op);
+
+	return (1);
+}

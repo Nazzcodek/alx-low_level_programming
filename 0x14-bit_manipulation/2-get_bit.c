9aee459e336a831051bf07c901c837aaedf5234c
Nazzcodek <bellnass09@gmail.com>
2023-04-10 02:18:58 -0700

    task 2 and 3

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * get_bit - A function that get bits
+ *
+ * @n: number of bit
+ * @index: index of bit
+ *
+ * Return: value of bit at index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+
+	if (!n && index < 64)
+		return (0);
+
+	for (; i <= 63; n >>= 1; i++)
+	{
+		if (index == i)
+			return (n && 1);
+	}
+	return (-1)
+}

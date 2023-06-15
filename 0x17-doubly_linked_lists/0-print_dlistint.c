e243ff1f79535f59cc4a7c29db75565b15df4898
Nazzcodek <bellnas09@gmail.com>
2023-06-15 04:20:26 -0700

    here we go

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print a double linked list
+ *
+ * @h: list to print
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h);
+{
+	size_t node_no = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		node_no++
+	}
+	return (node_no);
+}
---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - count length of a double linked list
+ *
+ * @h: list to count
+ *
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h);
+{
+	size_t node_no = 0;
+
+	while (h != NULL)
+	{
+		node_no++;
+		h = h->next;
+	}
+	return (node_no);
+}
